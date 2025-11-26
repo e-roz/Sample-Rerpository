65a9d5cd02fbdb16463eefa0e9bdf4e3b1dc3b7e
e-roz <137011618+e-roz@users.noreply.github.com>
2025-11-26 11:38:29 +0800

    Upload Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace StudentSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.tb_fn = new System.Windows.Forms.TextBox();
+            this.tb_ln = new System.Windows.Forms.TextBox();
+            this.cb_program = new System.Windows.Forms.ComboBox();
+            this.tb_age = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "StudentID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First name:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Program";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Age";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(16, 30);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(121, 20);
+            this.tb_id.TabIndex = 5;
+            // 
+            // tb_fn
+            // 
+            this.tb_fn.Location = new System.Drawing.Point(16, 83);
+            this.tb_fn.Name = "tb_fn";
+            this.tb_fn.Size = new System.Drawing.Size(121, 20);
+            this.tb_fn.TabIndex = 6;
+            // 
+            // tb_ln
+            // 
+            this.tb_ln.Location = new System.Drawing.Point(16, 122);
+            this.tb_ln.Name = "tb_ln";
+            this.tb_ln.Size = new System.Drawing.Size(121, 20);
+            this.tb_ln.TabIndex = 7;
+            // 
+            // cb_program
+            // 
+            this.cb_program.FormattingEnabled = true;
+            this.cb_program.Items.AddRange(new object[] {
+            "BSIT",
+            "BSHM",
+            "BSTM",
+            "BSBA"});
+            this.cb_program.Location = new System.Drawing.Point(16, 173);
+            this.cb_program.Name = "cb_program";
+            this.cb_program.Size = new System.Drawing.Size(121, 21);
+            this.cb_program.TabIndex = 8;
+            // 
+            // tb_age
+            // 
+            this.tb_age.Location = new System.Drawing.Point(16, 226);
+            this.tb_age.Name = "tb_age";
+            this.tb_age.Size = new System.Drawing.Size(121, 20);
+            this.tb_age.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(162, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(560, 282);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 29);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_age);
+            this.Controls.Add(this.cb_program);
+            this.Controls.Add(this.tb_ln);
+            this.Controls.Add(this.tb_fn);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.TextBox tb_fn;
+        private System.Windows.Forms.TextBox tb_ln;
+        private System.Windows.Forms.ComboBox cb_program;
+        private System.Windows.Forms.TextBox tb_age;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StudentSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =C:\\Users\\STI\\source\\repos\\StudentSystem\\StudentSystem\\studentInfo.mdf; Integrated Security = True");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO [Table] (StudentID, FirstName, LastName, Program, Age) VALUES (@StudentID, @FirstName, @LastName, @Program, @Age)", conn);
+                cmd.Parameters.AddWithValue("@StudentID", int.Parse(tb_id.Text));
+                cmd.Parameters.AddWithValue("@FirstName", tb_fn.Text);
+                cmd.Parameters.AddWithValue("@LastName", tb_ln.Text);
+                cmd.Parameters.AddWithValue("@Program", cb_program.Text);
+                cmd.Parameters.AddWithValue("@Age", int.Parse(tb_age.Text));
+                cmd.ExecuteNonQuery();
+
+                
+                conn.Close();
+                MessageBox.Show("Data Inserted");
+                load();
+            } catch (Exception ex )
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        public void load()
+        {
+            SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =C:\\Users\\STI\\source\\repos\\StudentSystem\\StudentSystem\\studentInfo.mdf; Integrated Security = True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [Table]", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try {
+                SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =C:\\Users\\STI\\source\\repos\\StudentSystem\\StudentSystem\\studentInfo.mdf; Integrated Security = True");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE [Table] SET  StudentID = @StudentID, FirstName = @FirstName, LastName = @LastName, Program = @Program, Age = @Age WHERE StudentID = @StudentID", conn);
+                cmd.Parameters.AddWithValue("@StudentID", int.Parse(tb_id.Text));
+                cmd.Parameters.AddWithValue("@FirstName", tb_fn.Text);
+                cmd.Parameters.AddWithValue("@LastName", tb_ln.Text);
+                cmd.Parameters.AddWithValue("@Program", cb_program.Text);
+                cmd.Parameters.AddWithValue("@Age", int.Parse(tb_age.Text));
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+                load();
+                MessageBox.Show("Updated Succesfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+    
+}
