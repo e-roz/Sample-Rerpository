ab5b456969cb035858f6ef9a5a9d52888042f1db
e-roz <137011618+e-roz@users.noreply.github.com>
2025-11-26 11:41:25 +0800

    Added Comment in program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace StudentSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1());//Run Apps blah blah
         }
     }
 }
